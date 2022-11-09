7e466644b1336b8ea12ed2aa66575e9cef8e7c40
Matt Bruer <mbruer@gmail.com>
2022-11-09 12:32:12 -0600

    updated navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {FaShoppingCart, FaHome,FaUser} from 'react-icons/fa'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaHome, FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store";
 
 const Navbar = () => {
-  return(
-    <div className='header'>
-      <Link to ="/home">
-      <img src= "/picture/logo.png"></img>
+  const isAuth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+  return (
+    <div className="header">
+      <Link to="/">
+        <img src="/picture/logo.png"></img>
       </Link>
-     
-      <ul className='nav-menu'>
-    <li><Link to ="/home">Home{'  '}<FaHome/> </Link></li>
-    <li><Link to ="/cart">Cart{'  '}<FaShoppingCart/> </Link></li>
-    <li><Link to ="/login">Login/SignUp{'  '}<FaUser/></Link></li>
+
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">
+            Home{"  "}
+            <FaHome />{" "}
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            Cart{"  "}
+            <FaShoppingCart />{" "}
+          </Link>
+        </li>
+        <li>
+          {isAuth.username ? (
+            <>
+              <p>{isAuth.username}</p>
+              <button
+                onClick={() => {
+                  logoutHandler();
+                }}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link to="/login">
+              Login/SignUp{"  "}
+              <FaUser />
+            </Link>
+          )}
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-
-export default Navbar
+export default Navbar;
