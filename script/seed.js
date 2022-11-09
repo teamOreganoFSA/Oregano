639ace49ff851aed3cd7f5403b1c583f3e48d83d
Erik Trejo <trejolujano.erik@gmail.com>
2022-11-09 11:48:07 -0800

    seed file

--- script/seed.js
+++ script/seed.js
@@ -1,49 +1,53 @@
-'use strict'
-
 const { green, red } = require('chalk');
-const { db, User, Product } = require('./server/db');
-
+const { db, models: { User, Product } } = require('../server/db');
 const seedUsers = [
   {
-    role: 'Admin',
     firstName: 'Adam',
     lastName: 'Min',
     email: 'ownersEmail@oregano.com',
-    password: 'password'
+    password: 'password',
+    address: '123 main st',
+    userType: 'ADMIN',
   },
   {
-    role: 'User',
     firstName: 'Clark',
     lastName: 'Kent',
     email: 'shopper123@random.com',
     password: 'password',
+    address: '1 oak rd',
+    userType: 'USER',
   }
 ]
-
 const seedProducts = [
   {
     name: 'Bamboo Pants',
     description: 'Pants made out of bamboo',
-    inventoryQty: 10,
+    inventoryQuantity: 10,
     price: 100,
     imageURL: 'http://www.sunprecautions.com/content/images/products/23600-1_9576_900x1200_100.jpg'
   },
   {
     name: 'Bamboo Longsleeve Shirt',
     description: 'Long sleeve shirt made out of bamboo',
-    inventoryQty: 20,
+    inventoryQuantity: 20,
     price: 80,
     imageURL: 'http://tshirtluver.com/image/cache/bamboo-t-shirt-tlts00011bambootshirt-800x885.jpg'
   },
   {
     name: 'Silkworm Shirt',
     description: 'Longsleeve shirt made from silk.',
-    inventoryQty: 35,
+    inventoryQuantity: 35,
     price: 120,
     imageURL: 'https://dm1ll01jrnse4.cloudfront.net/optimize/opti_755486098_img1.jpg'
+  },
+  {
+    name: 'Kale t-shirt',
+    description: 'Eco-friendly t-shirt made from kale.',
+    inventoryQuantity: 10,
+    price: 100,
+    imageURL: 'https://assets-global.website-files.com/5ccc8aa73871f9d0b1c81c04/61323cc015db8e5d998b1950_2099.jpg'
   }
 ]
-
 const seed = async () => {
   try {
     await db.sync({ force: true });
@@ -55,7 +59,6 @@ const seed = async () => {
     }))
   } catch (err) { console.error(err) }
 }
-
 module.exports = seed;
 // If this module is being required from another module, then we just export the
 // function, to be used as necessary. But it will run right away if the module
@@ -72,3 +75,13 @@ if (require.main === module) {
       db.close();
     });
 }
+(221 kB)
+http://www.sunprecautions.com/content/images/products/23600-1_9576_900x1200_100.jpg
+
+(230 kB)
+http://tshirtluver.com/image/cache/bamboo-t-shirt-tlts00011bambootshirt-800x885.jpg
+
+(41 kB)
+https://dm1ll01jrnse4.cloudfront.net/optimize/opti_755486098_img1.jpg
+
+(163 kB)
