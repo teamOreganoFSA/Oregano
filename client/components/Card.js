cd71e118ab14b207a15fbe8ac0cd6f6a06b1d178
LLU27 <107891664+LLU27@users.noreply.github.com>
2022-11-10 02:33:48 -0500

    added hover inner image function

--- client/components/Card.js
+++ client/components/Card.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
     <div>
-      <div className='card' data-aos="zoom-in"
-      data-aos-duration="1500">
-      <img className='card_image' src={props.imageURL}></img>
-      <div className='card_body'>
-        <h2 className='card_name'>{props.name}</h2>
-        <h3 className='card_price'>{props.price}</h3>
-        <h4 className='card_description'>{props.description}</h4>
-      <button className='card_button'>Add to Cart</button>
-        <Link to={`/products/single/${props.id}`}><button className='card_button' >View Product</button></Link>
+      <div className="card" data-aos="zoom-in" data-aos-duration="1500">
+        <div className="inner">
+          <img className="card_image" src={props.imageURL}></img>
+        </div>
+        <div className="card_body">
+          <h2 className="card_name">{props.name}</h2>
+          <h3 className="card_price">{props.price}</h3>
+          <h4 className="card_description">{props.description}</h4>
+          <button className="card_button">Add to Cart</button>
+          <Link to={`/products/single/${props.id}`}>
+            <button className="card_button">View Product</button>
+          </Link>
+        </div>
       </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- client/components/SingleProductViewCard.js
+++ client/components/SingleProductViewCard.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const SingleProductViewCard = (props) => {
   return (
-    <div>
+    <div class>
       <div className='card' data-aos="zoom-in"
       data-aos-duration="1500">
-      <img className='card_image' src={props.imageURL}></img>
+        <div className='inner'>
+        <img className='card_image' src={props.imageURL}></img>
+        </div>
       <div className='card_body'>
         <h2 className='card_name'>{props.name}</h2>
         <h3 className='card_price'>{props.price}</h3>
