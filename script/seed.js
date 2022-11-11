2ffe0181e0e77cc6321687de2a3094a1e2eda86d
jphwang212 <jphwang212@gmail.com>
2022-11-11 11:39:02 -0500

    fix seed file

--- script/seed.js
+++ script/seed.js
@@ -1,5 +1,5 @@
 const { green, red } = require('chalk');
-const { db, User, Product } = require('./server/db');
+const { db, models: { User, Product,Order,OrderProduct} } = require('../server/db');
 
 const seedUsers = [
   {
@@ -40,68 +40,6 @@ const seedUsers = [
 
 ]
 
-const seedProducts = [
-  {
-    name: 'Bamboo Pants',
-    description: 'Pants made out of bamboo',
-    inventoryQty: 10,
-    price: 100,
-    imageURL: 'http://www.sunprecautions.com/content/images/products/23600-1_9576_900x1200_100.jpg'
-  },
-  {
-    name: 'Bamboo Longsleeve Shirt',
-    description: 'Long sleeve shirt made out of bamboo',
-    inventoryQty: 20,
-    price: 80,
-    imageURL: 'http://tshirtluver.com/image/cache/bamboo-t-shirt-tlts00011bambootshirt-800x885.jpg'
-  },
-  {
-    name: 'Silkworm Shirt',
-    description: 'Longsleeve shirt made from silk.',
-    inventoryQty: 35,
-    price: 120,
-    imageURL: 'https://dm1ll01jrnse4.cloudfront.net/optimize/opti_755486098_img1.jpg'
-  },
-  {
-    name: 'Kale t-shirt',
-
-  }
-]
-
-const seed = async () => {
-  try {
-    await db.sync({ force: true });
-    await Promise.all(seedUsers.map(data => {
-      return User.create(data);
-    }))
-    await Promise.all(seedProducts.map(prod => {
-      return Product.create(prod);
-    }))
-  } catch (err) { console.error(err) }
-}
-
-=======
-const { db, models: { User, Product,Order,OrderProduct} } = require('../server/db');
-
-
-const seedUsers = [
-  {
-    firstName: 'Adam',
-    lastName: 'Min',
-    email: 'ownersEmail@oregano.com',
-    password: 'password',
-    address: '123 main st',
-    userType: 'ADMIN',
-  },
-  {
-    firstName: 'Clark',
-    lastName: 'Kent',
-    email: 'shopper123@random.com',
-    password: 'password',
-    address: '1 oak rd',
-    userType: 'USER',
-  }
-]
 
 // Products seed
 const seedProducts = [
