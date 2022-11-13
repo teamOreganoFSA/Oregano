056820d1f46fb6fc761729429a920c75308824a1
jphwang212 <jphwang212@gmail.com>
2022-11-13 17:31:14 -0500

    Added user add, edit, delete functions

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,42 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+// Add User // POST /api/users/
+// Use for admin adding users (may not need)
+// Expects req.body = {user fields}
+// Good place to add admin users
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.send({token: await user.generateToken()})
+  } catch (err) {
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        res.status(401).send('User already exists')
+      } else {
+        next(err)
+      }
+   }
+})
+
+// Edit user // PUT /api/users/:userId
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findByPk(userId);
+    if(user) {
+      await user.update(req.body);
+      res.send(user);
+    }
+  } catch (err) { next(err) }
+})
+
+// Delete user account
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    await User.destroy({ where: {
+      id: userId
+    }})
+  } catch (err) { next(err) }
+})
