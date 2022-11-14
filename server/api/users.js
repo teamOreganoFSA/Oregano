9d7489f790ad1c76569c915a73fbbc5d135f2ebb
jphwang212 <jphwang212@gmail.com>
2022-11-13 20:34:35 -0500

    template get route removed

--- server/api/users.js
+++ server/api/users.js
@@ -1,56 +1,63 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+const requireToken = async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    if (user.userType === "ADMIN") {
+      req.admin = user;
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
-})
+};
 
 // Add User // POST /api/users/
 // Use for admin adding users (may not need)
 // Expects req.body = {user fields}
 // Good place to add admin users
-router.post('/', async (req, res, next) => {
+router.post("/", requireToken, async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    res.send({token: await user.generateToken()})
+    res.send({ token: await user.generateToken() });
   } catch (err) {
-      if (err.name === 'SequelizeUniqueConstraintError') {
-        res.status(401).send('User already exists')
-      } else {
-        next(err)
-      }
-   }
-})
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    }
+  }
+});
 
 // Edit user // PUT /api/users/:userId
-router.put('/:userId', async (req, res, next) => {
+router.put("/:userId", requireToken, async (req, res, next) => {
   try {
     const userId = req.params.userId;
     const user = await User.findByPk(userId);
-    if(user) {
+    if (user) {
       await user.update(req.body);
       res.send(user);
     }
-  } catch (err) { next(err) }
-})
+  } catch (err) {
+    next(err);
+  }
+});
 
 // Delete user account
-router.delete('/:userId', async (req, res, next) => {
+router.delete("/:userId", requireToken, async (req, res, next) => {
   try {
     const userId = req.params.userId;
-    await User.destroy({ where: {
-      id: userId
-    }})
-  } catch (err) { next(err) }
-})
+    await User.destroy({
+      where: {
+        id: userId,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
