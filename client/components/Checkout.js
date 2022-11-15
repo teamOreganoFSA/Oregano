aee912fa5e9e57e8deb837438526515d45963d0d
jphwang212 <jphwang212@gmail.com>
2022-11-15 15:08:13 -0500

    completed checkout component

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,24 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./Cart";
+import { me } from "../store/auth";
+import { fetchCart } from "../store/cart";
 
 const Checkout = () => {
   // Possibly do amazon checkout style
   const { cart } = useSelector((state) => state);
-  const { user } = useSelector((state) => state);
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(me());
+    dispatch(fetchCart());
+  }, []);
+  console.log("Printing user: ", cart);
 
   return (
     <div>
       <h3>Checkout</h3>
       <div className="shipping">
         <h5>Shipping address</h5>
-        <p>{user.address}</p>
+        <p>{auth.address}</p>
       </div>
       <div className="payment">
         <h5>Payment method</h5>
-        <p>{user.id}</p>
+        <p>{auth.id}</p>
       </div>
       <div className="order">
         <h5>Review items and shipping</h5>
+        <Cart />
       </div>
       {/* hook up to place order */}
       <button>Place your order</button>
